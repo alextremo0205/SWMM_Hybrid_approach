97ec14a7ef45a7d37916ede610ce74d7c9535b31
Alexander G <51309784+alextremo0205@users.noreply.github.com>
2022-06-12 10:18:00 +0200

    Creation of test folder

---
+++ tests/test_emulator.py
@@ -0,0 +1,11 @@
+import unittest
+
+class EmulatorTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+    def tearDown(self):
+        pass
+
+    def test_Emulator(self):
+        self.assertTrue(0 == 0)

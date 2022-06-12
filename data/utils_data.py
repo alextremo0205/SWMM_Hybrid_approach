487061e5c579af8e5ded668c8411467421a88212
Alexander G <51309784+alextremo0205@users.noreply.github.com>
2022-06-12 02:13:20 +0200

    Improvement Database code
    Added runoff extraction. Multiple rainfall events (real and synthetic)

--- data/utils_data.py
+++ data/utils_data.py
@@ -1,19 +1,13 @@
+import os
 import yaml
-import numpy as np
-import pandas as pd
 import datetime
-import calendar
+import subprocess
+import numpy as np
 
 import plotly.graph_objects as go
-import plotly.express as px
-
-import swmmtoolbox.swmmtoolbox as swm
 
 
-import networkx as nx
-
-
-def import_config(config_file):
+def import_config_from_yaml(config_file):
     '''
     Creates configuration variables from file
     ------
@@ -22,12 +16,8 @@ def import_config(config_file):
     '''
     with open(config_file) as f:
         data = yaml.safe_load(f)
-        networks = data['dataset_names']
-        n_samples = data['n_samples']
-       
-        d_netw = {}
         
-    return networks, n_samples
+    return data
 
 # Tabulator ----------------------------------------------------------------------------------------------
 # From C:\Users\agarzondiaz\surfdrive\Year 2\Paper 2\PySWMM\utils.py
@@ -42,6 +32,30 @@ def tabulator(list_of_strings):
 
 # From rain generator code -------------------------------------------------------------------------------
 #C:\Users\agarzondiaz\surfdrive\Year 2\Paper 2\rain_generator.py
+
+def get_multiple_alt_blocks_rainfalls(n_rainfalls):
+  rainfalls = []
+  dt = 5
+  for i in range(n_rainfalls):
+    rand_A = np.random.randint(15000, 25000)
+    rand_B = np.random.uniform()*(20) + 30
+    rand_dur = np.random.randint(4,36)*dt
+
+    random_idf ={ 'A':rand_A,
+                  'B':rand_B,
+                  'n':1.09}
+
+    specs_rain = ['Synt_'+str(i), random_idf, rand_dur]
+    one_random_rain = altblocks(random_idf, dur = rand_dur, dt=dt)
+    # rainfalls.append((specs_rain, one_random_rain))
+    rainfalls.append(one_random_rain)
+    
+  return rainfalls
+
+
+
+
+
 #Time - dates conversor
 def conv_time(time):
   time/=60
@@ -51,13 +65,26 @@ def conv_time(time):
   return("%d:%02d" % (hours, minutes))
 
 # Modified function. Original taken from https://pyhyd.blogspot.com/2017/07/alternating-block-hyetograph-method.html
-def altblocks(idf,dur,dt):
+def altblocks(idf,dur,dt, dur_padding=60):
+    
+    value_padding = np.zeros(int(dur_padding/dt))
+    aPad = np.arange(dt,dur_padding+dt,dt)
+    aPostPad = np.arange(dur_padding+dur+dt,2*dur_padding+dur+dt,dt)
+
     aDur = np.arange(dt,dur+dt,dt)    # in minutes
     aInt = idf['A']/(aDur**idf['n'] + idf['B'])  # idf equation - in mm/h for a given return period
     aDeltaPmm = np.diff(np.append(0,np.multiply(aInt,aDur/60.0))) #Duration: min -> hours
     aOrd=np.append(np.arange(1,len(aDur)+1,2)[::-1],np.arange(2,len(aDur)+1,2))
+    
     prec = np.asarray([aDeltaPmm[x-1] for x in aOrd])
+
+
+    aDur = aDur+dur_padding
+    prec = np.concatenate((value_padding, prec, value_padding))
+    aDur = np.concatenate((aPad, aDur, aPostPad))
+
     prec_str = list(map(str, np.round(prec,2)))
+    
     aDur_time = list(map(conv_time, aDur))
     aDur_str = list(map(str, aDur_time))
     
@@ -106,6 +133,21 @@ def new_rain_lines(rainfall_dict, name_new_rain = 'name_new_rain'):
 
 
 #Formatter for rain text lines --------------------------------------------------------------------------------
+def create_datfiles(rainfalls, rainfall_dats_directory, identifier, isReal):
+  for idx, single_event in enumerate(rainfalls):
+    
+    if isReal:
+      string_rain = ["".join(i) for i in get_new_rain_lines_real(single_event, name_new_rain = 'R1') ]  
+    else:
+      string_rain = ["".join(i) for i in new_rain_lines_dat(single_event, name_new_rain = 'R1') ]
+
+    filename = '\\'+identifier+'_'+ str(idx)+'.dat'
+    with open(rainfall_dats_directory+filename, 'w') as f:
+        f.writelines(string_rain)
+
+
+
+
 #Taken from Colab: SWMM - INP Reader and INP Modifier.ipynb
 def new_rain_lines(rainfall_dict, name_new_rain = 'name_new_rain', day='1/1/2019'):
 
@@ -157,12 +199,73 @@ def get_lines_from_textfile(path):
   return lines_from_file
 
 
+def run_SWMM(inp_path, rainfall_dats_directory):
+  list_of_rain_datfiles = os.listdir(rainfall_dats_directory)
+
+
+  for event in list_of_rain_datfiles:
+      rain_event_path = rainfall_dats_directory + '\\' + event
+      
+      inp =  get_lines_from_textfile(inp_path)
+      dat =  get_lines_from_textfile(rain_event_path)
+      
+
+      for ln, line in enumerate(inp):
+          splitted_line_dat = dat[0].split('\t')
+          new_date = ''.join([splitted_line_dat[2], '/',splitted_line_dat[3],'/', splitted_line_dat[1]])
+          splitted_line_dat_last = dat[-1].split('\t')
+          new_last_date = ''.join([splitted_line_dat_last[2], '/',splitted_line_dat_last[3],'/', splitted_line_dat_last[1]])
+
+          new_last_time = ''.join([splitted_line_dat_last[4], ':',splitted_line_dat_last[5], ':', '00'])
+          if 'START_DATE' in line:
+              inp[ln] = line.replace(line.split()[-1], new_date)
+              
+          elif 'END_DATE' in line:
+              inp[ln] = line.replace(line.split()[-1], new_last_date)
+              
+          elif 'END_TIME' in line:
+              inp[ln] = line.replace(line.split()[-1], new_last_time)
+              
+
+          elif 'PLACEHOLDER1' in line:
+              inp[ln] = line.replace('PLACEHOLDER1', '\\'.join((rainfall_dats_directory, event)))
+              # print(inp[ln])
+
+              
+          # elif 'PLACEHOLDER2' in line:
+          #     inp[ln] = line.replace('PLACEHOLDER2', '\\'.join((rainfall_dats_directory, event, 'datfiles',
+          #                                                     'rain' +str(k[1]) + '.dat')))
+          # elif 'PLACEHOLDER3' in line:
+          #     inp[ln] = line.replace('PLACEHOLDER3', '\\'.join((rainfall_dats_directory, event, 'datfiles',
+          #                                                     'rain' +str(k[2]) + '.dat')))
+
+      nf = '\\'.join((simulations_path, event.replace('.dat', '')))
+      os.mkdir(nf)
+
+      with open(nf + '\\model.inp', 'w') as fh:
+          for line in inp:
+              fh.write("%s" % line)   
+
+
+      subprocess.run([r'C:\Program Files (x86)\EPA SWMM 5.1.015\swmm5.exe',
+                      nf+'\\model.inp',
+                      nf+'\\model.rpt',
+                      nf+'\\model.out'])
+
+      with open(nf + '\\'+ event, 'w') as fh:
+          for line in dat:
+              fh.write("%s" % line)
+
+
+
+
+
 
 
 
 
 # Plotting --------------------------------------------------------------------------------------------------------
-def hietograph(rain):
+def hietograph(rain, title):
   try:
     rain_times = [datetime.datetime.strptime(i,'%H:%M') for i in rain.keys()]
   except Exception:
@@ -175,7 +278,24 @@ def hietograph(rain):
   #     value=rain_values
   # ))
   bar = go.Bar(x=rain_times, y = rain_values) #px.bar(df, x='date', y="value")
-  return bar
+
+
+  fig= go.Figure()
+  fig.add_trace(bar)
+
+  fig.update_layout(
+      title=title,
+      xaxis_title="Time (h)",
+      yaxis_title="Intensity (mm/h)",
+      font=dict(
+          family="Times new roman",
+          size=18,
+      )
+  )
+
+
+
+  return fig
 
 
 
